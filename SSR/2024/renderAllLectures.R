f02b86d3cc96564b22bba439b0c0d965fd166a36
Johnny <j.b.vandoorn@uva.nl>
2025-10-01 12:20:10 +0200

    sign HA in t-test

--- SSR/2024/renderAllLectures.R
+++ SSR/2024/renderAllLectures.R
@@ -3,7 +3,7 @@ setwd("/Users/johnny/GitHubStuff/teaching-statistics/SSR/2025/")
 files <- list.files(full.names = TRUE, pattern = "\\.qmd", recursive = TRUE)
 files <- files[-length(files)]
 
-for (thisFile in files[c(2)]) {
+for (thisFile in files[c(1:2)]) {
   print(thisFile)
   quarto::quarto_render(thisFile)
 }
