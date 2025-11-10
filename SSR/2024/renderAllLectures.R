4052bc84be5db92cfa0e382c4eb1a416b6cb371c
Johnny <j.b.vandoorn@uva.nl>
2025-11-10 11:29:40 +0100

    omit r-code mediation lecture

--- SSR/2024/renderAllLectures.R
+++ SSR/2024/renderAllLectures.R
@@ -3,7 +3,7 @@ setwd("/Users/johnny/GitHubStuff/teaching-statistics/SSR/2025/")
 files <- list.files(full.names = TRUE, pattern = "\\.qmd", recursive = TRUE)
 files <- files[-length(files)]
 
-for (thisFile in files[8]) {
+for (thisFile in files[9]) {
   print(thisFile)
   quarto::quarto_render(thisFile)
 }
